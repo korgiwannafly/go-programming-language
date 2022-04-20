36bccd0ce07a8f0f5c644ed3c8b570d20dd2a0a7
korgiwannafly <korgiwannafly@gmail.com>
2022-04-20 16:54:41 +0700

    Slices in GO

--- chapter-4-composite-types/slices.go
+++ chapter-4-composite-types/slices.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	months := [...]string{
+		1: "January", 2: "February", 3: "March", 4: "April",
+		5: "May", 6: "June", 7: "Jully", 8: "August",
+		9: "September", 10: "October", 11: "November", 12: "December",
+	}
+
+	Q2 := months[4:7]
+	summer := months[6:9]
+
+	fmt.Println(Q2)
+	fmt.Println(summer)
 }
